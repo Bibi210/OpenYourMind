0da934145038886fc391ad8bd418fb3f63c0b784
Farouck <3407463>
2024-02-28 14:40:19 +0100

    Scrapping

--- backend/gutendex/admin.py
+++ backend/gutendex/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Author, Book, Format
 
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Format)
---
+++ backend/gutendex/management/commands/scrape_books.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+import requests
+from gutendex.models import Book, Author
+
+
+class Command(BaseCommand):
+    help = 'Scrape books from Gutendex and add them to the database'
+
+    def add_books_to_db(self, books_data):
+        for book_data in books_data:
+            authors = [Author.objects.get_or_create(name=author['name'])[0] for author in book_data['authors']]
+            book = Book.objects.create(
+                title=book_data['title'],
+                download_count=book_data['download_count']
+                # Ajoutez d'autres champs selon vos besoins
+            )
+            book.authors.set(authors)
+            book.save()
+            print(f'Added book: {book.title}')
+
+    def handle(self, *args, **kwargs):
+        base_url = "http://gutendex.com/books/"
+        books_added = 0
+
+        while books_added < 1664:
+            response = requests.get(base_url)
+            if response.status_code == 200:
+                books = response.json()['results']
+                self.add_books_to_db(books)
+                books_added += len(books)
+            else:
+                print('Failed to fetch books')
+                break
+
+            # Vérification du nombre de lignes pour chaque livre peut être complexe et nécessiter le téléchargement de chaque livre, ce qui n'est pas couvert dans cet exemple simplifié.
