bec4c2a62f46d08eb98d42ac258c0c855042ef41
Farouck <cherfifarouck10@gmail.com>
2024-02-29 14:37:01 +0100

    [BACKEND] Change Serializer to hit only Table Book

--- backend/gutendex/serializers.py
+++ backend/gutendex/serializers.py
@@ -13,14 +13,31 @@ class KeywordSerializer(serializers.ModelSerializer):
         model = Keyword
         fields = '__all__'
 
+class URLSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Format
+        fields = ['format_type', 'url']
+
+    def to_representation(self, instance):
+        if instance.format_type in ['text/plain; charset=us-ascii', 'text/plain; charset=utf-8','image/jpeg', 'image/png']:
+            return super().to_representation(instance)
+        else:
+            return {}
+
 
 class BookSerializer(serializers.ModelSerializer):
     authors = AuthorSerializer(many=True, read_only=True)
     keywords = KeywordSerializer(many=True, read_only=True)
+    urls = URLSerializer(many=True, read_only=True, source='formats')
 
     class Meta:
         model = Book
         fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['urls'] = [url for url in data['urls'] if url]
+        return data
 
 
 class BookKeywordSerializer(serializers.ModelSerializer):
