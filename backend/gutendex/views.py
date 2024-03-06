5a66620299fc01ed25e877fc0cde85da5dac424f
Farouck <cherfifarouck10@gmail.com>
2024-03-06 17:19:04 +0100

    [BackEnd] EndPoint

--- backend/gutendex/views.py
+++ backend/gutendex/views.py
@@ -1,4 +1,7 @@
+import nltk
+from django.db.models import Prefetch
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.response import Response
 from gutendex.serializers import *
 from .models import *
@@ -12,3 +15,19 @@ class TopBooks(APIView):
         top = Book.objects.order_by('-download_count')[:10]
         serializer = BookSerializer(top, many=True)
         return Response(serializer.data)
+
+
+class SearchBook(APIView):
+
+    def get(self, request, word):
+        try:
+            stemmer = nltk.stem.SnowballStemmer('english')
+            keyword = Keyword.objects.get(word=stemmer.stem(word.lower()))
+            bookkeywords = BookKeyword.objects.filter(keyword=keyword).order_by('-repetition_percentage')
+            books = [bk.book for bk in bookkeywords]
+            serializer = BookSerializer(books, many=True)
+            return Response(serializer.data)
+
+        except Keyword.DoesNotExist:
+            return Response({"error": "Keyword not found"}, status=status.HTTP_404_NOT_FOUND)
+
--- backend/src/urls.py
+++ backend/src/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from gutendex.views import TopBooks
+from gutendex.views import TopBooks, SearchBook
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('top-books/', TopBooks.as_view(), name='top-books'),
+    path('search/<str:word>', SearchBook.as_view(), name='search-book'),
 ]
