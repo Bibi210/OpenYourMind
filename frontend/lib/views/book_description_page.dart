3bab2f7358918a3da8900a89b4c357b88b866435
samarlahmar24 <samarlahmar24@gmail.com>
2024-03-07 15:24:30 +0100

    update mobile bookdescription

--- frontend/lib/views/book_description_page.dart
+++ frontend/lib/views/book_description_page.dart
@@ -4,92 +4,84 @@ import '../models/book.dart';
 
 class BookDescriptionPage extends StatelessWidget {
   final Book book;
-  const BookDescriptionPage({super.key,required this.book});
+  const BookDescriptionPage({super.key, required this.book});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const AppBarCustom(),
       body: SingleChildScrollView(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        children: [
-                          Image.network(book.imageUrl),
-                        ],
-                      ),
-                      const SizedBox(width: 50), // Add spacing between columns
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Text(
-                            'Chapter List:',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 10),
-                          SizedBox(
-                            width: 500,
-                            child: Card(
-                              surfaceTintColor:
-                                  Theme.of(context).colorScheme.surface,
-                              elevation: 3,
-                              margin: const EdgeInsets.all(5),
-                              child: SizedBox(
-                                height: MediaQuery.of(context).size.width * 0.3,
-                                width: MediaQuery.of(context).size.width * 0.3,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(16.0),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        'Title: ${book.title}',
-                                        style: const TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 8),
-                                      Text(
-                                        'Authors: ${book.authors.elementAt(0)["name"].toString()}',
-                                        style: const TextStyle(
-                                          fontSize: 16,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 8),
-                                      Text(
-                                        'Subjects: ${book.subjects}',
-                                        style: const TextStyle(
-                                          fontSize: 16,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 8),
-                                      Text(
-                                        'Bookshelves: ${book.bookshelves.join(", ")}',
-                                        style: const TextStyle(
-                                          fontSize: 16,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 0,
+                  height: 0,
+                  child: Image.network(book.imageUrl),
+                ),
+                const SizedBox(width: 50), // Add spacing between columns
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: Card(
+                    surfaceTintColor: Theme.of(context).colorScheme.surface,
+                    elevation: 3,
+                    margin: const EdgeInsets.all(5),
+                    child: SizedBox(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              book.title,
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                          ),
-                        ],
+                            const SizedBox(height: 8),
+                            Text(
+                              'Authors: ${book.authors.elementAt(0)["name"].toString()}',
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              'Subjects: ${book.subjects}',
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              'Bookshelves: ${book.bookshelves.join(", ")}',
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
+                    ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
+          ],
+        ),
+      ),
     );
   }
 }
